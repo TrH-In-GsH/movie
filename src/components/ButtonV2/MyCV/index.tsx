8f87b46e52f1028b4fd36c95746cdddcedb1055e
Nguyen Thanh Phong <phongntgcd210422@fpt.edu.vn>
2024-02-01 21:15:58 +0700

    First commit

--- src/components/ButtonV2/MyCV/index.tsx
+++ src/components/ButtonV2/MyCV/index.tsx
@@ -8,8 +8,8 @@ import CvPersonalSkill from "../CvPersonalSkill";
 
 const MyCV = () => {
   return (
-    <div className="flex w-200 mt-2 mb-20 drop-shadow-xl bg-white rounded	">
-      <div className="col_left w-[350px] bg-indigo-600 flex flex-col items-center	drop-shadow-xl rounded">
+    <div className="flex w-200 mt-2 mb-20 drop-shadow-2xl bg-white rounded	">
+      <div className="col_left w-[350px] bg-indigo-600 flex flex-col items-center	drop-shadow-2xl ">
         <img className="w-[210px] h-[210px] rounded-full mt-10" src="https://i.wpimg.pl/1280x/filerepo.grupawp.pl/api/v1/display/embed/617a50b7-514b-4944-aac8-3dca16aa2aec" alt="" />
         <CVContract/>
         <CVExpertise />
